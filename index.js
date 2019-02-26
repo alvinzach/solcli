a8cf5ef19f060af572ac885bf40181af665d2a54
alvinzach <alvinzach@gmail.com>
2019-02-26 13:18:41 +0530

    added deployment code

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 const program=require('commander')
 const solc=require('solc')
 const fs=require('fs')
-
+var Web3=require('web3');
 program
     .version('1.0.0')
     .description('solidity compiler')
-    .option('-p,--port <n>','rpc port')
-    .option('-h,--host <n>','rpc host')
-
+    
 program
     .command('init <filename>')
     .description('initiate')
     .alias('i')
+    .option('-p,--port <n>','rpc port')
+    .option('-h,--host <n>','rpc host')
     .action((filename)=>{
         var config={
             "smartcontracts":[]
@@ -42,7 +42,7 @@ program
                         console.log(err)
                         return
                     }else{
-                        console.log('run "npm install web3" ')
+                        console.log(`Edit smartcontracts/${filename}.sol file`)
                     }
                 })
                 
@@ -87,5 +87,46 @@ program
             console.log('no contract specified')
         }
     })
-
+program
+    .command('deploy [filename]')
+    .description('deploy smart contract')
+    .alias('d')
+    .option('-a,--address <n>','Address of sender')
+    .option('-p,--password <n>','password')
+    .action((filename)=>{
+        var config=JSON.parse(fs.readFileSync('config.json'))
+        var web3=new Web3(new Web3.providers.HttpProvider(`${config.host}:${config.port}`));
+        if(program.address){
+            web3.eth.defaultAccount=address;
+        }else{
+            web3.eth.defaultAccount=web3.eth.accounts[0];
+        }
+        if(program.password){
+            web3.personal.unlockAccount(web3.eth.defaultAccount,program.password,100000)
+        }
+        var contract_details=JSON.parse(fs.readFileSync(`artifacts/${filename}.json`));
+        var contract=web3.eth.contract(JSON.parse(contract_details.abi))
+        const contractInstance = contract.new({
+            data: '0x' + contract_details.bytecode,
+            gas: 90000*5
+        }, (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+        
+            console.log("transaction hash :"+res.transactionHash);
+        
+            var intId=setInterval(()=>{
+                if(contractInstance.address!==undefined){
+                    contract_details.address=contractInstance.address;
+                    fs.writeFileSync(`artifacts/${filename}.json`,JSON.stringify(contract_details,null,2))
+                    console.log(contractInstance.address)
+                    clearInterval(intId)
+                    return
+                }
+            },3000)
+        });
+        console.log(config)
+    })
 program.parse(process.argv)
